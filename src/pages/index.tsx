5c6ae2ca5d6b68a726dc6eac57af72594c83b4d8
Thiago Bignotto <bignotto@gmail.com>
2021-10-24 06:53:23 -0300

    project started

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import type { NextPage } from "next";
+import { Flex, Stack, Button, Input } from "@chakra-ui/react";
+
+import { db } from "../services/firebase";
+import { collection, addDoc } from "firebase/firestore";
+
+const Home: NextPage = () => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleClick = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Stack spacing="4">
+        <Input
+          name="email"
+          type="email"
+          label="E-Mail"
+          value={value}
+          onChange={handleChange}
+        />
+      </Stack>
+      <Button
+        type="submit"
+        mt="6"
+        colorScheme="pink"
+        onClick={(e) => handleClick(e)}
+      >
+        Entrar
+      </Button>
+    </Flex>
+  );
+};
+
+export default Home;
